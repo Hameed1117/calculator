0045586210c51fa3299b095e0310d9d261f8ca20
Hameed1117 <hameed2k1117@gmail.com>
2025-02-09 22:48:37 -0500

    added pylint code and added basic version of code in main branch

---
+++ tests/test_cal.py
@@ -0,0 +1,17 @@
+"""Contains Tests for different Calc Functions"""
+from apps.cal import add, div, mul, sub
+def test_add():
+    """test addition functionality"""
+    assert add(2,2)==4
+
+def test_sub():
+    """test subtraction functionality"""
+    assert sub(7,5)==2
+
+def test_mul():
+    """test multiplication functionality"""
+    assert mul(7,5)==35
+
+def tes_div():
+    """test division functionality"""
+    assert div(4,2)==2
